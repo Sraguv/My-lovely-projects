18045a565a0c43fa53c577efdc672c2acceb13ed
Sraguv <yalecher@gmail.com>
2022-05-30 20:42:00 +0300

    DDD

--- my_app.py
+++ my_app.py
@@ -1,1 +1,19 @@
-# напиши здесь код основного приложения и первого экрана
+from instr import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+    def setappear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+app = QApplication([])
+main_win = QWidget()
+main_win.show()
+app.exec_()
