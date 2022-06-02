5e8e98660c7dde5ba98098d4bb362a3798fa4b86
Sraguv <yalecher@gmail.com>
2022-06-02 21:14:09 +0300

    upd

--- final_win.py
+++ final_win.py
@@ -1,1 +1,11 @@
-# напиши здесь код третьего экрана приложения
+from instr import *
+from final_win import *
+from PyQt5.QtCore import Qt, QTime, QTimer
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
+class FinalWin(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        #self.exp = exp
+        pass
--- my_app.py
+++ my_app.py
@@ -1,3 +1,4 @@
+from second_win import *
 from instr import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
@@ -5,7 +6,11 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLay
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-    def setappear(self):
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
@@ -13,7 +18,17 @@ class MainWindow(QWidget):
         self.hello_text = QLabel(txt_hello)
         self.instruction = QLabel(txt_instruction)
         self.button = QPushButton(txt_next)
+        self.v_line = QVBoxLayout()
+        self.v_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.button, alignment = Qt.AlignCenter)
+        self.setLayout(self.v_line)
+    def connects(self): 
+        self.button.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
+
 app = QApplication([])
-main_win = QWidget()
-main_win.show()
+main_win = MainWindow()
 app.exec_()
